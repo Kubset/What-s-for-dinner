bdad6796a84609385100432263961e9bf28a1199
Kubset <kuba1228334@gmail.com>
2018-07-21 18:04:21 +0200

    add commands for create data

---
+++ src/resources/database/CreateData.sql
@@ -0,0 +1,34 @@
+CREATE TABLE main_dish (
+    dish_id SERIAL PRIMARY KEY,
+    name TEXT,
+    favourite INTEGER
+);
+
+
+CREATE TABLE soup (
+    soup_id SERIAL PRIMARY KEY,
+    name TEXT,
+    favourite INTEGER
+);
+
+--PRIMARY KEY (soup(soup_id), main_dish(dish_id));
+
+
+CREATE TABLE components (
+    component_id SERIAl PRIMARY KEY,
+    name TEXT
+ );
+
+CREATE TABLE dish_components (
+    dish_id INTEGER REFERENCES main_dish(dish_id),
+    component_id INTEGER REFERENCES components(component_id),
+    PRIMARY KEY(dish_id, component_id)
+);
+
+CREATE TABLE soup_components (
+    soup_id INTEGER REFERENCES soup(soup_id),
+    component_id INTEGER REFERENCES components(component_id),
+    PRIMARY KEY(soup_id, component_id)
+);
+
+
