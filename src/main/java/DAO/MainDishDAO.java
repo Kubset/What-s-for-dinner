c47d771f7627dc4a1e26be84a2319e81a488349b
Kubset <kuba1228334@gmail.com>
2018-07-21 18:48:02 +0200

    add skeleton DAO to MainDish table

---
+++ src/main/java/DAO/MainDishDAO.java
@@ -0,0 +1,33 @@
+package DAO;
+
+import Model.MainDish;
+
+import java.util.List;
+
+public class MainDishDAO implements DAO<MainDish> {
+
+    @Override
+    public void add(MainDish Entity) {
+
+    }
+
+    @Override
+    public void update(MainDish Entity) {
+
+    }
+
+    @Override
+    public void delete(MainDish Entity) {
+
+    }
+
+    @Override
+    public MainDish get(int ID) {
+        return null;
+    }
+
+    @Override
+    public List<MainDish> get() {
+        return null;
+    }
+}
