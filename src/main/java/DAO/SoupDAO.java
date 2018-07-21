1e2ecf845dcc38ba0581d18f39389b10774c884f
Kubset <kuba1228334@gmail.com>
2018-07-21 18:48:15 +0200

    add skeleton DAO to Soup table

---
+++ src/main/java/DAO/SoupDAO.java
@@ -0,0 +1,34 @@
+package DAO;
+
+import Model.Soup;
+
+import java.util.List;
+
+public class SoupDAO implements DAO<Soup> {
+
+    @Override
+    public void add(Soup Entity) {
+
+    }
+
+    @Override
+    public void update(Soup Entity) {
+
+    }
+
+    @Override
+    public void delete(Soup Entity) {
+
+    }
+
+    @Override
+    public Soup get(int ID) {
+        return null;
+    }
+
+    @Override
+    public List<Soup> get() {
+        return null;
+    }
+
+}
