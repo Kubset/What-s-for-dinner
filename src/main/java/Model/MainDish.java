7e3270ff5290673990911fa0b3ed70e3ebf3bbd8
Kubset <kuba1228334@gmail.com>
2018-07-23 13:02:21 +0200

    add Id and constructors

--- src/main/java/Model/MainDish.java
+++ src/main/java/Model/MainDish.java
@@ -3,6 +3,26 @@ package Model;
 public class MainDish {
     private String name;
     private int favourite;
+    private int id;
+
+    public MainDish(String name, int favourite) {
+        this.name = name;
+        this.favourite = favourite;
+    }
+
+    public MainDish(int id, String name, int favourite) {
+        this.id = id;
+        this.favourite = favourite;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
