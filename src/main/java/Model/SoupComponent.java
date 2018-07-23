f7bcbdb6972f41915aedca18d7d1487a5654978c
Kubset <kuba1228334@gmail.com>
2018-07-23 13:39:34 +0200

    add implemented soupComponent model

---
+++ src/main/java/Model/SoupComponent.java
@@ -0,0 +1,31 @@
+package Model;
+
+public class SoupComponent {
+    private int SoupId;
+    private int ComponentId;
+    private int id;
+
+    public int getSoupId() {
+        return SoupId;
+    }
+
+    public void setSoupId(int soupId) {
+        SoupId = soupId;
+    }
+
+    public int getComponentId() {
+        return ComponentId;
+    }
+
+    public void setComponentId(int componentId) {
+        ComponentId = componentId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
