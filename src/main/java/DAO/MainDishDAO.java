72420427421837c9ea80daf9fba601c298e7cd38
Kubset <kuba1228334@gmail.com>
2018-07-23 12:59:37 +0200

    implement statements

--- src/main/java/DAO/MainDishDAO.java
+++ src/main/java/DAO/MainDishDAO.java
@@ -1,6 +1,7 @@
 package DAO;
 
 import Model.MainDish;
+import sun.applet.Main;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -15,16 +16,23 @@ public class MainDishDAO extends AbstractDAO<MainDish> {
 
     @Override
     void fillStatementToAddData(MainDish entity) throws SQLException {
+        super.preparedStatement.setString(1, entity.getName());
+        super.preparedStatement.setInt(2, entity.getFavourite());
     }
 
 
     @Override
     void fillStatementToEditData(MainDish entity) throws SQLException {
+        super.preparedStatement.setString(1, entity.getName());
+        super.preparedStatement.setInt(2, entity.getFavourite());
+        super.preparedStatement.setInt(3,entity.getId());
     }
 
 
     @Override
     void fillStatementToDeleteData(MainDish entity) throws SQLException {
+        super.preparedStatement.setInt(1, entity.getId());
     }
+
 
 }
