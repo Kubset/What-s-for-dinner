cf3f87296c8e574c7233d54f8311a65b6310324c
Kubset <kuba1228334@gmail.com>
2018-09-04 17:36:11 +0200

    fix problem with data lost, make backup

--- src/main/webapp/assets/scripts/addComponentRow.js
+++ src/main/webapp/assets/scripts/addComponentRow.js
@@ -1,4 +1,37 @@
-function addComponentRow() {
-    
+function addNewComponent() {
+    var xhr= new XMLHttpRequest();
+    xhr.open('GET', '/assets/html/newComponent.html', true);
+    xhr.onreadystatechange= function() {
+        if (this.readyState!==4) return;
+        if (this.status!==200) return; // or whatever error handling you want
+        document.getElementById('component-rows').innerHTML += this.responseText;
+    };
+    xhr.send();
+}
 
+function addMainContent(path) {
+    var xhr= new XMLHttpRequest();
+    xhr.open('GET', path, true);
+    xhr.onreadystatechange= function() {
+        if (this.readyState!==4) return;
+        if (this.status!==200) return; // or whatever error handling you want
+        document.getElementById('main-content').innerHTML += this.responseText;
+    };
+    xhr.send();
+}
+
+
+window.onload = function() {
+    var path = window.location.pathname
+
+    switch(path) {
+        case '/soup/add':
+            addMainContent("/assets/html/addSoupContent.html")
+            break;
+
+        case '/dish/add':
+            addMainContent("/assets/html/addDishContent.html")
+            break;
+
+    }
 }
