48c6884af7e443ef8566e9fadbb1b93d64664e37
Kubset <kuba1228334@gmail.com>
2018-09-05 13:25:14 +0200

    add empty methods mapToJson

--- src/main/java/Mappers/DishComponentMapper.java
+++ src/main/java/Mappers/DishComponentMapper.java
@@ -4,14 +4,28 @@ import Model.DishComponent;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class DishComponentMapper implements Mapper<DishComponent> {
 
+    @Override
     public DishComponent map(ResultSet resultSet) throws SQLException {
             int id = resultSet.getInt("dish_component_id");
             int componentId = resultSet.getInt("component_id");
             int DishId = resultSet.getInt("dish_id");
 
             return new DishComponent(id, componentId, DishId);
+    }
+
+    @Override
+    public String mapToJson(List<DishComponent> entities) {
+        //TODO: not implemented yet
+        return null;
+    }
+
+    @Override
+    public String mapToJson(DishComponent entity) {
+        //TODO: not implemented yet
+        return null;
     }
 }
