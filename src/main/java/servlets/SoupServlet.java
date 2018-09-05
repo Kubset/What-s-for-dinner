b53295a61c0ba07e84a90653bc0b0c5b578d5471
Kubset <kuba1228334@gmail.com>
2018-09-05 15:04:59 +0200

    add partly implmented post method

--- src/main/java/servlets/SoupServlet.java
+++ src/main/java/servlets/SoupServlet.java
@@ -1,11 +1,20 @@
 package servlets;
 
+import DAO.ComponentDAO;
+import Model.Component;
+import Model.Soup;
+import Model.SoupComponent;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 public class SoupServlet extends HttpServlet {
 
@@ -17,7 +26,23 @@ public class SoupServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+
+
+        String soupName = req.getParameter("soup-name");
+
+        String[] componentNames = req.getParameterValues("component");
+        String[] count = req.getParameterValues("count");
+        String[] unit = req.getParameterValues("unit");
+
+        List<Component> components = new ArrayList<>();
+        List<SoupComponent> soupComponents = new ArrayList<>();
+        Soup soup = new Soup(soupName);
+
+        //TODO: have to implement services !!
+
+        resp.getWriter().write("saved");
+
+
     }
 
     @Override
