a8ee48ed6a608cc0be0990d112eb2681cf7da482
Kubset <kuba1228334@gmail.com>
2018-09-09 14:33:45 +0200

    implement method which get all soups from database

--- src/main/webapp/assets/scripts/injectElements.js
+++ src/main/webapp/assets/scripts/injectElements.js
@@ -54,5 +54,25 @@ function addSuggestComponents() {
 }
 
 
+function getSoupsFromDatabase() {
+    var soups = []
+    var xhr= new XMLHttpRequest();
+    xhr.open('GET', "/api/soup", false);
+    xhr.onreadystatechange = function() {
+        if (this.readyState!==4) return;
+        if (this.status!==200) return;
+        var json = JSON.parse(this.responseText);
+        var node;
+        json.forEach(element => {
+            soups.push(element["name"])
+        });
+    };
+    xhr.send();
+
+    return soups;
+}
+
+
+
 
 
