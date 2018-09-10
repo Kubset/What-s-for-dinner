23e8e79126d57d016a60ab043160391cf61efbad
Kubset <kuba1228334@gmail.com>
2018-09-10 20:43:39 +0200

    update method responsible for build prepared statement

--- src/main/java/DAO/SoupComponentDAO.java
+++ src/main/java/DAO/SoupComponentDAO.java
@@ -8,8 +8,8 @@ import java.sql.SQLException;
 public class SoupComponentDAO extends AbstractDAO<SoupComponent>{
 
     public SoupComponentDAO() {
-        ADD_QUERY = "INSERT INTO soup_components (soup_id, component_id) VALUES (?, ?);";
-        EDIT_QUERY = "UPDATE soup_components SET soup_id=?, component_id=? WHERE soup_component_id=?;";
+        ADD_QUERY = "INSERT INTO soup_components (soup_id, component_id, count, unit) VALUES (?, ?, ?, ?);";
+        EDIT_QUERY = "UPDATE soup_components SET soup_id=?, component_id=?, count=?, unit=? WHERE soup_component_id=?;";
         DELETE_QUERY = "DELETE FROM soup_components WHERE soup_component_id=?;";
         super.mapper = new SoupComponentMapper();
     }
@@ -18,13 +18,17 @@ public class SoupComponentDAO extends AbstractDAO<SoupComponent>{
     void fillStatementToAddData(SoupComponent entity) throws SQLException {
         super.preparedStatement.setInt(1, entity.getSoupId());
         super.preparedStatement.setInt(2, entity.getComponentId());
+        super.preparedStatement.setInt(3, entity.getCount());
+        super.preparedStatement.setString(4, entity.getUnit().getName());
     }
 
 
     void fillStatementToEditData(SoupComponent entity) throws SQLException {
         super.preparedStatement.setInt(1, entity.getSoupId());
         super.preparedStatement.setInt(2,entity.getComponentId());
-        super.preparedStatement.setInt(3, entity.getId());
+        super.preparedStatement.setInt(3,entity.getCount());
+        super.preparedStatement.setString(4,entity.getUnit().getName());
+        super.preparedStatement.setInt(5, entity.getId());
     }
 
 
