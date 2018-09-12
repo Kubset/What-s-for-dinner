a04d264fce5f73881035ab42fe359119809a1bcd
Kubset <kuba1228334@gmail.com>
2018-09-13 00:38:17 +0200

    implement POST method

--- src/main/java/servlets/DishServlet.java
+++ src/main/java/servlets/DishServlet.java
@@ -1,11 +1,20 @@
 package servlets;
 
+import Model.Component;
+import Model.MainDish;
+import Model.Soup;
+import Model.Unit;
+import services.DishManager;
+import services.SoupManager;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DishServlet extends HttpServlet {
 
@@ -17,7 +26,27 @@ public class DishServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        String dishName = req.getParameter("dish-name");
+
+        String[] componentNames = req.getParameterValues("component");
+        String[] count = req.getParameterValues("count");
+        String[] unit = req.getParameterValues("unit");
+
+        List<Component> components = new ArrayList<>();
+        for(int i=0; i<count.length; i++) {
+            components.add(new Component(componentNames[i],
+                    Integer.parseInt(count[i]),
+                    new Unit(unit[i])));
+        }
+
+        MainDish mainDish = new MainDish(dishName);
+        mainDish.setComponents(components);
+
+        DishManager dishManager = new DishManager();
+
+        dishManager.create(mainDish);
+
+        resp.sendRedirect(req.getHeader("referer"));
     }
 
     @Override
