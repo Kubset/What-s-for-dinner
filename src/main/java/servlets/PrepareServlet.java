5bcad6b5b2c12b939f60041f29414a2e10b65a02
Kubset <kuba1228334@gmail.com>
2018-09-14 15:20:35 +0200

    add prepare servlet with partly implemented POST method

---
+++ src/main/java/servlets/PrepareServlet.java
@@ -0,0 +1,48 @@
+package servlets;
+
+import Model.Soup;
+import services.SoupManager;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class PrepareServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/dashBoard.html");
+        if (dispatcher != null) dispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String[] checkBoxes = req.getParameterValues("day");
+
+
+        SoupManager soupManager = new SoupManager();
+        List<Soup> randomSoups = soupManager.getRandom(checkBoxes.length);
+
+        //TODO: process dinner, implement dishManager
+
+        resp.sendRedirect(req.getHeader("referer"));
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPut(req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doDelete(req, resp);
+    }
+}
+
