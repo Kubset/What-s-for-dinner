da5bdac0a2f6c36f0879c117cc07cdd19f620384
Kubset <kuba1228334@gmail.com>
2018-09-16 20:05:44 +0200

    make generic getCollectionFromDatabase function and small refactor of variable scopes

--- src/main/webapp/assets/scripts/customFunctions.js
+++ src/main/webapp/assets/scripts/customFunctions.js
@@ -1,22 +1,22 @@
 Array.prototype.contains = function ( needle ) {
-    for (i in this) {
-        if (this[i] == needle) return true;
+    for (let i in this) {
+        if (this[i] === needle) return true;
     }
     return false;
- }
+ };
 
 
-function getCollectionFromDatabase(collectionName) {
-    var elements = []
-    var xhr= new XMLHttpRequest();
+
+function getCollectionFromDatabase(collectionName,attribute) {
+    let elements = [];
+    let xhr= new XMLHttpRequest();
     xhr.open('GET', "/api/"+ collectionName, false);
     xhr.onreadystatechange = function() {
         if (this.readyState!==4) return;
         if (this.status!==200) return;
-        var json = JSON.parse(this.responseText);
-        var node;
+        let json = JSON.parse(this.responseText);
         json.forEach(element => {
-            elements.push(element["name"])
+            elements.push(element[attribute])
         });
     };
     xhr.send();
