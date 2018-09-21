bb0d354d4a42b201e8c29d479865b05516cc213a
Kubset <kuba1228334@gmail.com>
2018-09-21 16:55:52 +0200

    add function responsible for send shopping list and skeleton function for send recipe list

--- src/main/webapp/assets/scripts/customFunctions.js
+++ src/main/webapp/assets/scripts/customFunctions.js
@@ -138,4 +138,33 @@ function postDinnerCollection() {
 
 }
 
+function sendShoppingList() {
+    let form = document.getElementById("main-form");
+    let hiddenNode = document.createElement("input");
+    hiddenNode.setAttribute("hidden", "true");
+    hiddenNode.setAttribute("name", "components");
+    let nodeComponents = document.getElementsByClassName("component-list")
 
+    let components = "";
+    let tempComponents;
+    for(let i=0; i<nodeComponents.length; i++) {
+        tempComponents = nodeComponents[i].getElementsByClassName("component");
+        tempComponents = Array.prototype.map.call(tempComponents, e => e = e.innerHTML);
+
+        components += tempComponents.toString();
+        components += ",";
+    }
+    components = components.substring(0,components.length-1);
+    hiddenNode.setAttribute("value", components);
+
+    if(!document.getElementsByTagName("input").length) {
+        form.appendChild(hiddenNode);
+    }
+    form.submit();
+}
+
+function sendRecipeList() {
+   //TODO: not implemented yet
+    let form = document.getElementById("main-form");
+    form.submit();
+}
