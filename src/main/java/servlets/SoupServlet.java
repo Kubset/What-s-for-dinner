08143fccc984d5a620c2a8de525026105fc0fae5
Kubset <kuba1228334@gmail.com>
2018-09-24 21:43:49 +0200

    implement doPut method, handle doPut in doPost method

--- src/main/java/servlets/SoupServlet.java
+++ src/main/java/servlets/SoupServlet.java
@@ -28,34 +28,59 @@ public class SoupServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String soupName = req.getParameter("soup-name");
+        String[] URL = req.getRequestURI().toString().split("/");
+        if(URL[2].equals("edit")) {
+            doPut(req,resp);
+        } else {
+            String soupName = req.getParameter("soup-name");
 
-        String[] componentNames = req.getParameterValues("component");
-        String[] count = req.getParameterValues("count");
-        String[] unit = req.getParameterValues("unit");
-        String recipe = req.getParameterValues("recipe")[0];
+            String[] componentNames = req.getParameterValues("component");
+            String[] count = req.getParameterValues("count");
+            String[] unit = req.getParameterValues("unit");
+            String recipe = req.getParameterValues("recipe")[0];
 
-        List<Component> components = new ArrayList<>();
-        for(int i=0; i<count.length; i++) {
-           components.add(new Component(componentNames[i],
-                                        Integer.parseInt(count[i]),
-                                        new Unit(unit[i])));
+            List<Component> components = new ArrayList<>();
+            for (int i = 0; i < count.length; i++) {
+                components.add(new Component(componentNames[i],
+                        Integer.parseInt(count[i]),
+                        new Unit(unit[i])));
+            }
+
+            Soup soup = new Soup(soupName);
+            soup.setComponents(components);
+            soup.setRecipe(recipe);
+
+            SoupManager soupManager = new SoupManager();
+            soupManager.create(soup);
+
+            resp.sendRedirect(req.getHeader("referer"));
         }
-
-        Soup soup = new Soup(soupName);
-        soup.setComponents(components);
-        soup.setRecipe(recipe);
-
-        SoupManager soupManager = new SoupManager();
-        soupManager.create(soup);
-
-        resp.sendRedirect(req.getHeader("referer"));
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPut(req, resp);
+        int soupId = Integer.parseInt(req.getParameter("id"));
+        String soupName = req.getParameter("soup-name");
+        String[] componentNames = req.getParameterValues("component");
+        String[] count = req.getParameterValues("count");
+        String[] unit = req.getParameterValues("unit");
+        String recipe = req.getParameter("recipe");
+
+        List<Component> components = new ArrayList<>();
+        for (int i = 0; i < count.length; i++) {
+            components.add(new Component(componentNames[i],
+                    Integer.parseInt(count[i]),
+                    new Unit(unit[i])));
+        }
+
+        Soup soup = new Soup(soupId,soupName,components);
+        soup.setRecipe(recipe);
+        SoupManager soupManager = new SoupManager();
+        soupManager.edit(soup);
+
+        resp.sendRedirect(req.getHeader("referer"));
     }
+
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
