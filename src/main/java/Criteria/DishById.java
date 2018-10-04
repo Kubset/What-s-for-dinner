4f2e7346db4e5340a35a7f18c49565d3d87faa82
Kubset <kuba1228334@gmail.com>
2018-10-04 14:17:25 +0200

    refactor classes move duplicated method to abstract

--- src/main/java/Criteria/DishById.java
+++ src/main/java/Criteria/DishById.java
@@ -6,30 +6,11 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class DishById implements SqlCriteria {
+public class DishById extends GetById {
 
-    private final String QUERY = "SELECT * FROM main_dish WHERE dish_id=?;";
-    private int id;
+    private static final String QUERY = "SELECT * FROM main_dish WHERE dish_id=?;";
 
     public DishById(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public PreparedStatement toPreparedStatement() throws SQLException {
-        PreparedStatement preparedStatement = null;
-        Connection connection = null;
-        try {
-            connection = ConnectionProvider.getConnection();
-            preparedStatement = connection.prepareStatement(QUERY);
-            preparedStatement.setInt(1, this.id);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.err.println("Can't perform this query due to " +
-                    "exception occurance when creating PreparedStatement");
-        }
-
-        return preparedStatement;
+        super(QUERY, id);
     }
 }
--- src/main/java/Criteria/DishComponentByDishId.java
+++ src/main/java/Criteria/DishComponentByDishId.java
@@ -6,29 +6,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public class DishComponentByDishId implements SqlCriteria{
-    private final String QUERY = "SELECT * FROM dish_components WHERE dish_id=?;";
-    private int id;
+public class DishComponentByDishId extends GetById{
+    private static final String QUERY = "SELECT * FROM dish_components WHERE dish_id=?;";
 
     public DishComponentByDishId(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public PreparedStatement toPreparedStatement() throws SQLException {
-        PreparedStatement preparedStatement = null;
-        Connection connection = null;
-        try {
-            connection = ConnectionProvider.getConnection();
-            preparedStatement = connection.prepareStatement(QUERY);
-            preparedStatement.setInt(1, this.id);
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-            System.err.println("Can't perform this query due to " +
-                    "exception occurance when creating PreparedStatement");
-        }
-
-        return preparedStatement;
+        super(QUERY, id);
     }
 }
