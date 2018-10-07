280ed3f98148e678b19103631ac1fe3e2045e81b
Kubset <kuba1228334@gmail.com>
2018-10-07 10:15:13 +0200

    clean unused methods, now responsible of MainServlet is only to start application

--- src/main/java/servlets/MainServlet.java
+++ src/main/java/servlets/MainServlet.java
@@ -11,23 +11,9 @@ public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
         RequestDispatcher dispatcher = req.getRequestDispatcher("/dashBoard.html");
         if (dispatcher != null) dispatcher.forward(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
-    }
-
-    @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPut(req, resp);
-    }
-
-    @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doDelete(req, resp);
     }
 }
 
