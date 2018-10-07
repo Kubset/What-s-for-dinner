f36038d9079703c188f1268b3c4605a92add3ae2
Kubset <kuba1228334@gmail.com>
2018-10-07 20:04:11 +0200

    create soup service test class ,initialize beforeAll and all necessary imports

---
+++ src/test/java/services/SoupManagerTest.java
@@ -0,0 +1,32 @@
+package services;
+
+import Criteria.AllComponents;
+import Criteria.SoupsByName;
+import DAO.ComponentDAO;
+import DAO.ConnectionProvider;
+import DAO.MainDishDAO;
+import DAO.SoupDAO;
+import Model.Component;
+import Model.Soup;
+import Model.Unit;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SoupManagerTest {
+
+    private static ComponentDAO componentDAO = new ComponentDAO();
+    private static SoupDAO soupDAO = new SoupDAO();
+    private static MainDishDAO mainDishDAO = new MainDishDAO();
+    private static SoupManager soupManager = new SoupManager();
+    private static ComponentManager componentManager = new ComponentManager();
+    private static DishManager dishManager = new DishManager();
+
+    @BeforeAll
+    public static void setup() {
+        ConnectionProvider.setPropertiesPath("src/test/resources");
+    }
