7c203ba3b0ddb0074bb91f8b31039291158a5455
Kubset <kuba1228334@gmail.com>
2018-10-07 10:45:29 +0200

    validateFrom function main refactor, use separated methods

--- src/main/webapp/assets/scripts/formValidator.js
+++ src/main/webapp/assets/scripts/formValidator.js
@@ -1,62 +1,95 @@
 class FormValidator {
-    static validateAddForm(formName) {
+
+    static validateForm(formName) {
         let isValidate = true;
-
+        let requestMethod = this.validateTypeOfForm();
         let databaseManager = new DatabaseManager();
-        let form = document.getElementById('main-form');
-        let components = form.getElementsByTagName('input');
-        let recipe = document.getElementById('recipe');
 
-        if (components.length < 3) {
+        let mealName = document.getElementsByTagName("input")[0];
+        let recipe = document.getElementById("recipe");
+        let id = document.getElementsByName("id")[0];
+
+        let componentNames = document.getElementsByName("component")
+        let componentUnits = document.getElementsByName("unit");
+        let componentCounts = document.getElementsByName("count");
+
+        if(!this.validateNumberComponents(componentNames))
             isValidate = false;
-            ContentInjector.addAlertMessage("You have to add at least one component", "alert-danger")
+
+        //TODO: name meal validate makes application slow
+        // if(requestMethod === "POST") {
+        //     if(!this.validateMealInDatabase(mealName, formName)) {
+        //         isValidate = false;
+        //     }
+        // }
+
+        if(!this.validateEmptyFields(componentNames)) {
+            isValidate = false;
         }
 
-        if(!document.getElementsByName("id").length) {
-            let soups = databaseManager.getCollectionFromDatabase(formName, "name");
-            if (soups.contains(components[0].value)) {
-                isValidate = false;
-                ContentInjector.addAlertMessage("Meal with this name already exist", "alert-danger");
-            }
+        if(!this.validateEmptyFields(componentCounts)) {
+            isValidate = false;
         }
-
-
-        for (let i = 0; i < components.length; i++) {
-            if (!components[i].value.length) {
-                isValidate = false;
-                ContentInjector.addAlertMessage("There is at least one empty meal or component field", "alert-danger")
-            }
-        }
-
 
         if(!recipe.value.length) {
-            isValidate = false;
             ContentInjector.addAlertMessage("There is empty recipe area", "alert-danger")
+            isValidate = false;
         }
 
-
-        for(let i=0; i<components.length; i++) {
-            if(!this.validateByRegex(components[i].value)) {
-                ContentInjector.addAlertMessage("Illegal input data, You can't use special characters like:\n" +
-                    "@#!$%^&*~<>/\\", "alert-danger");
-                isValidate = false;
-            }
+        if(!mealName.value.length) {
+            ContentInjector.addAlertMessage("You have empty meal field", "alert-danger");
+            isValidate =  false;
         }
 
         if (!this.validateByRegex(recipe.value)) {
-            ContentInjector.addAlertMessage("Illegal input data, You can't use special characters like:\n" +
-                                            "@#!$%^&*~<>/\\", "alert-danger");
             isValidate = false;
         }
 
+        for(let i=0; i<componentNames.length; i++) {
+            if(!this.validateByRegex(componentNames[i].value))
+                isValidate = false;
+        }
 
+        if(!this.validateByRegex(mealName.value)) {
+            isValidate = false;
+        }
+
+        if(!this.validateByRegex(recipe.value)) {
+            isValidate = false;
+        }
 
         if (isValidate) {
             ContentInjector.addAlertMessage("Successfully added to database", "alert-primary");
             setTimeout(function () {
-                form.submit()
+                let json = {}
+                let jsonComponents = {};
+                json["name"] = mealName.value;
+                json["recipe"] = recipe.value;
+                json["favourite"] = -1;
+                json["components"] = [];
+
+                for(let i=0; i<componentNames.length; i++) {
+                    let component = {};
+                    component["name"] = componentNames[i].value;
+                    component["count"] = componentCounts[i].value;
+                    component["unit"] = {};
+                    component["unit"]["name"] = componentUnits[i].value;
+                    json["components"][i] = component;
+                }
+
+
+                if(id !== undefined) {
+                    json["id"] = id.value;
+                    databaseManager.editElementInDatabase(formName, json)
+                } else {
+                    databaseManager.addElementToDatabase(formName, json);
+                }
+                location.reload();
             }, 2000);
         }
+
+
+
     }
 
     static validatePrepareForm() {
